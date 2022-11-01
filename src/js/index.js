65269566405372d9e77e2b68c91af6b223333613
david <2508808197@qq.com>
2022-11-01 19:07:55 +0800

    11/01

--- src/js/index.js
+++ src/js/index.js
@@ -33,20 +33,85 @@ $(document).ready(function () {
     var centerX = 500;
     var centerY = 500;
 
-    var circleCnt =0
+    var circleCnt = 0
+
+    //是否正在移动
+    var isMoving = false
     $drawCir = $("#circle")
-    $drawCir.click(function (event) {
-        circleCnt=circleCnt+1
-        var circlename='circle_'+circleCnt
+    $drawCir.click(function (event) {//click事件创建圆
+        circleCnt = circleCnt + 1
+        var circlename = 'circle_' + circleCnt
         $circle = $('<div class="circle"></div>');
-        $circle.attr('id',circlename);
+        $circle.attr('id', circlename);
         $draw.append($circle);
-        var radius=100
+        var radius = 100//半径
         $circle.css("left", centerX - radius + "px");
         $circle.css("top", centerY - radius + "px");
         $circle.css("width", 2 * radius + "px");
         $circle.css("height", 2 * radius + "px");
         $circle.css("border-radius", radius + "px")
+        //为新创建的圆添加事件
+        $("#" + circlename).on("click", function () {
+            $("#style").addClass("active in")
+            $("#text").removeClass("active")
+            $("#yangshi").addClass("active")
+            $("#wenben").removeClass("active")
+            $("#xiankuan").val("" + $(this).css("border-width"))
+            $("#xianxing").val("" + $(this).css("border-style"))
+            $("#xianse").val("" + $(this).css("border-color"))
+            $("tianchongse").val("" + $(this).css("background-color"))
+        })
+        //按下鼠标开始画圆
+        // $("#"+circlename).on("mousedown",function(event){
+        //     centerX=event.pageX-$draw.offset().left;//相对于画图面板的距离
+        //     centerY=event.pageY-$draw.offset().top;
+        //     isMoving=true;
+        //     event.preventDefault();
+        // })
+        // //鼠标移动事件
+        // $("#"+circlename).on("mousemove",function(){
+
+
+        // })
+        // //释放鼠标事件 
+        // $("#"+circlename).on("mouseup",function(event){
+        //     if(isMoving){
+        //         $("#"+circlename).css("left",event.pageX-$draw.offset().left)
+        //         $("#"+circlename).css("top",event.pageY-$draw.offset().top)
+        //     }
+        //     isMoving=false
+        // })
+        $("#" + circlename).mousedown(function (event) {
+            deltax = event.clientX - $(this).offset().left //可视距离左-距离父级的左
+            deltay = event.clientY - $(this).offset().top   //
+            $(document).bind('mousemove', function (event) {
+                x = event.clientX - deltax
+                y = event.clientY - deltay
+                centerX = x + radius
+                centerY = y + radius
+                // if (x < deltax) {
+                //     x = deltax
+                // }
+                // if (x > ($draw.width() - radius)) {
+                //     x = $draw.width() - radius
+                // }
+                // if (y < deltay) {
+                //     y = deltay
+                // }
+                // if (y > ($draw.height() - radius)) {
+                //     y = $draw.height() - radius
+                // }
+                console.log(x,y)
+                console.log(deltax,deltay)
+                $("#" + circlename).css({ 'left': x + 'px', 'top': y + 'px' })
+                return false
+            })
+            $(document).bind('mouseup', function (event) {
+                $(this).unbind('mousemove')
+                $(this).unbind('mouseup')
+            })
+            return false
+        })
     })
-    
+
 })
